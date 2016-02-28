b46c7e568438b463fc5fd6c92255ed5b2a9db2b0
Dyeudiel <bucaro345@hotmail.com>
2016-02-28 17:08:15 -0500

    cn
    correcion del form conexion

--- ElectivaProfesionalII/ElectivaProfesionalII/Mensajes/ConexionDB.Designer.vb
+++ ElectivaProfesionalII/ElectivaProfesionalII/Mensajes/ConexionDB.Designer.vb
@@ -22,48 +22,48 @@ Partial Class ConexionDB
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.btnReintentar = New System.Windows.Forms.Button()
         Me.btnSalir = New System.Windows.Forms.Button()
+        Me.lblInfo = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(280, 26)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "No hay conexión con la Base de Datos." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Favor de clic en Reintentar o contacte a s" &
-    "u administrador."
         '
         'btnReintentar
         '
-        Me.btnReintentar.Location = New System.Drawing.Point(72, 66)
+        Me.btnReintentar.Location = New System.Drawing.Point(69, 60)
         Me.btnReintentar.Name = "btnReintentar"
         Me.btnReintentar.Size = New System.Drawing.Size(75, 23)
-        Me.btnReintentar.TabIndex = 1
+        Me.btnReintentar.TabIndex = 0
         Me.btnReintentar.Text = "Reintentar"
         Me.btnReintentar.UseVisualStyleBackColor = True
         '
         'btnSalir
         '
-        Me.btnSalir.Location = New System.Drawing.Point(154, 65)
+        Me.btnSalir.Location = New System.Drawing.Point(151, 60)
         Me.btnSalir.Name = "btnSalir"
         Me.btnSalir.Size = New System.Drawing.Size(75, 23)
-        Me.btnSalir.TabIndex = 2
+        Me.btnSalir.TabIndex = 1
         Me.btnSalir.Text = "Salir"
         Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.Location = New System.Drawing.Point(13, 13)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(259, 26)
+        Me.lblInfo.TabIndex = 2
+        Me.lblInfo.Text = "La Conexión con el servidor de Bases de Datos fallo, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "de clic en Reintentar o co" &
+    "ntacte a su administrador."
         '
         'ConexionDB
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(308, 100)
+        Me.ClientSize = New System.Drawing.Size(280, 95)
+        Me.Controls.Add(Me.lblInfo)
         Me.Controls.Add(Me.btnSalir)
         Me.Controls.Add(Me.btnReintentar)
-        Me.Controls.Add(Me.Label1)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "ConexionDB"
@@ -74,7 +74,7 @@ Partial Class ConexionDB
 
     End Sub
 
-    Friend WithEvents Label1 As Label
     Friend WithEvents btnReintentar As Button
     Friend WithEvents btnSalir As Button
+    Friend WithEvents lblInfo As Label
 End Class
---
+++ ElectivaProfesionalII/ElectivaProfesionalII/Mensajes/ConexionDB.vb
@@ -0,0 +1,9 @@
+﻿Public Class ConexionDB
+    Private Sub ConexionDB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
