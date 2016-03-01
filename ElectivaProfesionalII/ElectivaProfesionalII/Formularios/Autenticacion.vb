5eb2542d57bfd3003de5edf79ec504ebd17e0ef9
hsanchez2016 <hectorfabians@gmail.com>
2016-03-01 16:09:40 -0500

    Actualizacion
    Se actualiza para validar cambios

--- ElectivaProfesionalII/ElectivaProfesionalII/Formularios/Autenticacion.vb
+++ ElectivaProfesionalII/ElectivaProfesionalII/Formularios/Autenticacion.vb
@@ -1,3 +1,13 @@
 ﻿Public Class FormAutenticacion
+    Private Sub FormAutenticacion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Picture_ingresar_Click(sender As Object, e As EventArgs) Handles Picture_ingresar.Click
+
+    End Sub
+
+    Private Sub Picture_salir_Click(sender As Object, e As EventArgs) Handles Picture_salir.Click
+        End
+    End Sub
 End Class
